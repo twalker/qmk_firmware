e09a876d9a4dc53cf7c96b9e85f5820289da703f
tim walker <tim.twalker@gmail.com>
2021-07-04 21:24:46 -0700

    Mirror tapping term from oryx

--- keyboards/kyria/keymaps/twalker/rules.mk
+++ keyboards/kyria/keymaps/twalker/rules.mk
@@ -2,3 +2,9 @@ OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
 MOUSEKEY_ENABLE = yes
+
+# Mirror Oryx tapping settings
+TAPPING_TERM = 200
+IGNORE_MOD_TAP_INTERRUPT = yes
+TAPPING_FORCE_HOLD = no    # Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+PERMISSIVE_HOLD = no
