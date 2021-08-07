a8a17b67bd34dccfc901dba80f49508568275b66
Tim Walker <tiwalker@starbucks.com>
2021-08-06 20:56:24 -0700

    fix backspace

--- keyboards/kyria/keymaps/twalker/keymap.c
+++ keyboards/kyria/keymaps/twalker/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *                        `----------------------------------'  `----------------------------------'
     */
     [QWERTY] = LAYOUT(
-      TD(TD_TBCP),   KC_Q,   KC_W,     KC_E,   KC_R,   KC_T,                                           KC_Y,   KC_U,   KC_I,   KC_O,      KC_P, KC_BSLS,
+      TD(TD_TBCP),   KC_Q,   KC_W,     KC_E,   KC_R,   KC_T,                                           KC_Y,   KC_U,   KC_I,   KC_O,      KC_P, KC_BSPC,
       KC_ESC, HOME_A, HOME_S,   HOME_D, HOME_F,   KC_G,                                           KC_H, HOME_J, HOME_K, HOME_L, HOME_SCLN, KC_QUOT,
       KC_NO,    KC_Z,   KC_X,     KC_C,   KC_V,   KC_B, KC_NO, KC_NO,      KC_NO, KC_NO, KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_ENT,
                        KC_NO,  MO(NUM), MO(SYM), MO(NAV), MO(WIN),  TG(MOUSE), LT(NAV, KC_SPC), KC_NO,  KC_NO, KC_NO
