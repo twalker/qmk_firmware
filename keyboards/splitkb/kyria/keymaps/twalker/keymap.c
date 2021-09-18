2b511a7adc15f4b4b4d006b42d5f3cff03dfb625
tim walker <tim.twalker@gmail.com>
2021-09-18 10:39:39 -0700

    move querty toggle to BL

--- keyboards/splitkb/kyria/keymaps/twalker/keymap.c
+++ keyboards/splitkb/kyria/keymaps/twalker/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
     * |  ESC   |   A  |   R  |   S  |   T  |   G  |                              |   M  |   N  |   E  |   I  |  O   |  ' "   |
     * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
-    * |        |   Z  |   X  |   C  |   D  |   V  |      | WIN  |  | MS   | CDH  |   K  |   H  | ,  < | . >  | /  ? | Enter  |
+    * | QWERTY |   Z  |   X  |   C  |   D  |   V  |      | WIN  |  | MS   |      |   K  |   H  | ,  < | . >  | /  ? | Enter  |
     * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
     *                        |      |      |      |      |      |  |      | Space|      |      |      |
     *                        |      | Num  | Sym  | Nav  | BSPC |  | DEL  | Nav  |      |      |      |
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [CDH] = LAYOUT(
       TD(TD_TBCP),  KC_Q,   KC_W,     KC_F,   KC_P,   KC_B,                                       KC_J,   KC_L,   KC_U,   KC_Y,    KC_SCLN, KC_BSPC,
       KC_ESC, CDHHM_A, CDHHM_R,   CDHHM_S, CDHHM_T,   KC_G,                                       KC_M, CDHHM_N, CDHHM_E, CDHHM_I, CDHHM_O, KC_QUOT,
-      KC_NO,    KC_Z,   KC_X,     KC_C,   KC_D,   KC_V, KC_NO, MO(WIN),      MO(MOUSE), TG(QWERTY), KC_K,    KC_H, KC_COMM, KC_DOT, KC_SLSH,  KC_ENT,
+      TG(QWERTY),    KC_Z,   KC_X,     KC_C,   KC_D,   KC_V, KC_NO, MO(WIN),      MO(MOUSE), KC_NO, KC_K,    KC_H, KC_COMM, KC_DOT, KC_SLSH,  KC_ENT,
                        KC_NO,  MO(NUM), MO(SYM), MO(NAV), KC_BSPC,           KC_DEL, LT(NAV, KC_SPC), KC_NO,  KC_NO, KC_NO
     ),
 
@@ -139,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,  KC_F2,    KC_F3,   KC_F4,   _______,                                     KC_NO,    KC_7,    KC_8,    KC_9,   KC_NO, _______,
     _______, KC_F5,  KC_F6,    KC_F7,   KC_F8,   _______,                                     KC_NO,    KC_4,    KC_5,    KC_6,   KC_NO, _______,
     _______, KC_F9,   KC_F10,  KC_F11, KC_F12,   _______, _______, _______, _______, _______, KC_NO,    KC_1,    KC_2,    KC_3,   KC_NO, _______,
-                                _______, _______, _______, _______, _______, _______, _______, KC_0,  KC_DOT, _______
+                                _______, _______, _______, _______, _______, _______,  KC_NO, KC_0,  KC_DOT, _______
     ),
     /*
     * Window managment
