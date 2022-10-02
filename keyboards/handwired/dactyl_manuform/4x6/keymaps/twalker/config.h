8b9b1d17c4f50585e62d99fbc07930a49920a06c
twalker <tim.twalker@gmail.com>
2022-10-02 08:40:51 -0700

    Invert trackball Y

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/twalker/config.h
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/twalker/config.h
@@ -1,23 +1,3 @@
-/*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
 /* Use I2C or Serial, not both */
@@ -46,3 +26,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // failed: B1, B0 -- success with F7 (Y is reversed)
 #define PMW33XX_CS_PIN F7
 
+// Pointing device is on the right split.
+#define POINTING_DEVICE_RIGHT
+
+// Limits the frequency that the sensor is polled for motion.
+#define POINTING_DEVICE_TASK_THROTTLE_MS 1
+
+#define POINTING_DEVICE_INVERT_X
+#define ROTATIONAL_TRANSFORM_ANGLE  -25
