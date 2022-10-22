6c2f56955aff73c1c63675cc5bc4ca4547b5ab3c
twalker <tim.twalker@gmail.com>
2022-10-22 10:45:04 -0700

    Add mouse wheel on left

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/twalker/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/twalker/keymap.c
@@ -215,7 +215,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case WIN_L:
       if (record->event.pressed) {
         if (user_config.is_macos) {
-          SEND_STRING(SS_LCTRL(SS_LALT(SS_TAP(X_LEFT))));
+          SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_LEFT))));
         } else {
           // Pop OS
           SEND_STRING(SS_LGUI(SS_LSFT(SS_TAP(X_LEFT))));
@@ -225,7 +225,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case WIN_R:
       if (record->event.pressed) {
         if (user_config.is_macos) {
-          SEND_STRING(SS_LCTRL(SS_LALT(SS_TAP(X_RGHT))));
+          SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_RGHT))));
         } else {
           SEND_STRING(SS_LGUI(SS_LSFT(SS_TAP(X_RGHT))));
         }
@@ -234,25 +234,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case WIN_U:
       if (record->event.pressed) {
         if (user_config.is_macos) {
-          SEND_STRING(SS_LCTRL(SS_LALT(SS_TAP(X_UP))));
+          SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_UP))));
         } else {
-          SEND_STRING(SS_LGUI(SS_LCTRL(SS_TAP(X_UP))));
+          SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_UP))));
         }
       }
       return false;
     case WIN_D:
       if (record->event.pressed) {
         if (user_config.is_macos) {
-          SEND_STRING(SS_LCTRL(SS_LALT(SS_TAP(X_DOWN))));
+          SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_DOWN))));
         } else {
-          SEND_STRING(SS_LGUI(SS_LCTRL(SS_TAP(X_DOWN))));
+          SEND_STRING(SS_LGUI(SS_LCTL(SS_TAP(X_DOWN))));
         }
       }
       return false;
     case WIN_FUL:
       if (record->event.pressed) {
         if (user_config.is_macos) {
-          SEND_STRING(SS_LCTRL(SS_LALT(SS_TAP(X_ENTER))));
+          SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_ENTER))));
         } else {
           SEND_STRING(SS_LGUI(SS_TAP(X_M)));
         }
@@ -263,7 +263,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case WIN_LG:
       if (record->event.pressed) {
         if (user_config.is_macos) {
-          SEND_STRING(SS_LCTRL(SS_LALT(SS_TAP(X_EQL))));
+          SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_EQL))));
         } else {
           SEND_STRING(SS_LGUI(SS_TAP(X_ENTER)) SS_LSFT(SS_TAP(X_RGHT)) SS_TAP(X_ESC));
         }
@@ -272,7 +272,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case WIN_SM:
       if (record->event.pressed) {
         if (user_config.is_macos) {
-          SEND_STRING(SS_LCTRL(SS_LALT(SS_TAP(X_MINS))));
+          SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_MINS))));
         } else {
           SEND_STRING(SS_LGUI(SS_TAP(X_ENTER)) SS_LSFT(SS_TAP(X_LEFT)) SS_TAP(X_ESC));
         }
@@ -381,21 +381,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [MAC] = LAYOUT(
     //=======* =======* =======* =======* =======* =======*          =======* =======* =======* =======* =======* =======*
-        RESET, _______, _______, _______, DM_PLY1, DM_PLY2,          _______, _______,USERNAME, _______, _______,   RESET,
+      QK_BOOT, _______, _______, _______, DM_PLY1, DM_PLY2,          _______, _______,USERNAME, _______, _______, QK_BOOT,
       _______, _______, _______, DM_RSTP, _______, _______,         MACOS_TG, _______,   EMAIL, _______, _______, _______,
       _______, _______,_______,_______,_______,_______,              _______, _______, _______, _______, _______, _______,
     //                  =======* =======*                                              =======* =======*                
                         DM_REC1, DM_REC2,                                              _______, _______,
                                           _______, _______,                   _______,
                                           _______, _______,                   _______,
-                                         _______, _______,          _______, _______
+                                          _______, _______,          _______, _______
     //                                    =======* =======*          =======* =======*
     ),
 
     [MSE] = LAYOUT(
     //=======* =======* =======* =======* =======* =======*          =======* =======* =======* =======* =======* =======*
-      _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
-      TO(CDH), _______, _______, _______, _______, _______,          _______, KC_BTN1, KC_BTN2, KC_BTN3, _______, _______,
+      _______, _______, _______, KC_WH_D, _______, _______,          _______, _______, _______, _______, _______, _______,
+      TO(CDH), _______, KC_WH_L, KC_WH_U, KC_WH_R, _______,          _______, KC_BTN1, KC_BTN2, KC_BTN3, _______, _______,
       _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
     //                  =======* =======*                                              =======* =======*                
                         _______, _______,                                              _______, _______,
