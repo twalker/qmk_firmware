b29971a1882730e1db6f171d5ef3a53602bdf490
twalker <tim.twalker@gmail.com>
2022-12-10 19:23:51 -0800

    every key prints a char

--- keyboards/handwired/dactyl_manuform/3x6_3/keymaps/twalker/keymap.c
+++ keyboards/handwired/dactyl_manuform/3x6_3/keymaps/twalker/keymap.c
@@ -28,11 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                 └───┤NAV│       │NAV├───┘
       *                     └───┘       └───┘
       */
-    [0] = LAYOUT_split_3x6_3(
-        KC_NO,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,
-        KC_NO,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_NO,
-        KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,
-                                KC_LGUI, KC_BSPC,  KC_SPC,    KC_SPC,  KC_ENT,  KC_RALT
+    [CDH] = LAYOUT_split_3x6_3(
+        KC_1,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_4,
+        KC_2,   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,       KC_O,    KC_5,
+        KC_3,   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H, KC_COMM,  KC_DOT,    KC_SLSH,    KC_6,
+                                  KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F
     )
 };
 
