9e0f420b719d441fc2288a6f8b77581388d17150
twalker <tim.twalker@gmail.com>
2023-01-08 11:00:50 -0800

    Faster homerow mods on Corny dactyl

--- keyboards/handwired/dactyl_manuform/3x6_3/keymaps/twalker/config.h
+++ keyboards/handwired/dactyl_manuform/3x6_3/keymaps/twalker/config.h
@@ -4,10 +4,15 @@
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
-#define TAPPING_TERM 150
+// #define TAPPING_TERM 150
+// #define TAPPING_TERM_PER_KEY
+// #define TT_SHIFT 120
+// #define TT_ALT 180
+
+#define TAPPING_TERM 120
 #define TAPPING_TERM_PER_KEY
-#define TT_SHIFT 120
-#define TT_ALT 180
+#define TT_SHIFT 110
+#define TT_ALT 150
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
