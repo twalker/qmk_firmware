2ccccfe3acf3d9a707c982094ce773dcb8f5fb43
twalker <tim.twalker@gmail.com>
2023-02-26 15:03:48 -0800

    Add piantor

---
+++ keyboards/beekeeb/piantor/keymaps/twalker/config.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+#define TAPPING_TERM 150
+#define TAPPING_TERM_PER_KEY
+#define TT_SHIFT 110
+#define TT_ALT 160
+
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD true
+#define TAPPING_FORCE_HOLD_PER_KEY
+
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
+ 
+// COLEMAK DH homerow mods
+// Left-hand
+#define CDHHM_A LCTL_T(KC_A)
+#define CDHHM_R LALT_T(KC_R)
+#define CDHHM_S LGUI_T(KC_S)
+#define CDHHM_T LSFT_T(KC_T)
+// Right-hand
+#define CDHHM_N RSFT_T(KC_N)
+#define CDHHM_E RGUI_T(KC_E)
+#define CDHHM_I LALT_T(KC_I)
+#define CDHHM_O RCTL_T(KC_O)
+
