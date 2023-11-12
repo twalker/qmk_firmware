ac0f6c5ff09c64671114fc24d5579d6e27ee5f12
twalker <tim.twalker@gmail.com>
2023-11-11 17:46:08 -0800

    Add oleds and underglow to ejc's board

--- keyboards/lily58/keymaps/ejc/config.h
+++ keyboards/lily58/keymaps/ejc/config.h
@@ -45,10 +45,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
-
 // Underglow
-/*
+
 #undef RGBLED_NUM
-#define RGBLED_NUM 14    // Number of LEDs
+#define RGBLED_NUM 12    // Number of LEDs
 #define RGBLIGHT_SLEEP
-*/
--- keyboards/lily58/keymaps/ejc/keymap.c
+++ keyboards/lily58/keymaps/ejc/keymap.c
@@ -1,5 +1,14 @@
 #include QMK_KEYBOARD_H
 
+#ifdef RGBLIGHT_ENABLE
+void keyboard_post_init_user(void) {
+  rgblight_enable_noeeprom(); // Enables RGB, without saving settings
+  rgblight_sethsv_noeeprom(HSV_PURPLE);
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+}
+#endif
+
+
 enum layer_number {
   _QWERTY = 0,
   _LOWER,
