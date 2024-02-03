743be3c1322d3a1a8b4e55544c51dfdda5fbc2f9
tim walker <tim.twalker@gmail.com>
2024-02-03 15:26:30 -0800

    Add SYM layer

--- keyboards/totem/keymaps/twalker/keymap.c
+++ keyboards/totem/keymaps/twalker/keymap.c
@@ -25,8 +25,8 @@
 // └─────────────────────────────────────────────────┘
 
 enum layers {
-    CDH = 0
-    // SYM,
+    CDH = 0,
+    SYM,
     // NAV,
     // NUM,
     // WIN,
@@ -63,10 +63,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [CDH] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
               KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,      KC_J,     KC_L,     KC_U,     KC_Y,     KC_QUOT,
-             CTL_A,    ALT_R,    GUI_S,    SHT_T,     KC_G,      KC_M,     SHT_N,    GUI_E,    ALT_I,    CTL_O,
+              CTL_A,    ALT_R,    GUI_S,    SHT_T,    KC_G,      KC_M,     SHT_N,    GUI_E,    ALT_I,    CTL_O,
    KC_ESC,    KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,      KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_SCLN,
-                                  KC_NO,   KC_ESC,    KC_BSPC,    KC_SPC,   KC_ENT,    KC_BSPC
- )
+                                  KC_NO,    KC_ESC,   KC_BSPC,   KC_SPC,   KC_ENT,   KC_BSPC
+ ),
+
+   [SYM] = LAYOUT(
+ //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
+              KC_EXLM,  KC_AT,    KC_LCBR,  KC_RCBR,  KC_PIPE,   KC_AMPR,  KC_ASTR,  KC_LT,    KC_GT,    KC_QUOT,
+              KC_HASH,  KC_DLR,   KC_LPRN,  KC_RPRN,  KC_GRV,    KC_MINS,  KC_EQL,   KC_PLUS,  KC_UNDS,  KC_SCLN,
+    KC_NO,    KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,   KC_BSLS,  KC_COLN, KC_COMM,   KC_DOT,   KC_SLSH,   KC_NO,
+                                  KC_NO,    KC_ESC,   KC_BSPC,   KC_SPC,   KC_ENT,   KC_BSPC
+ ),
 
 /*
    ╺━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╸
