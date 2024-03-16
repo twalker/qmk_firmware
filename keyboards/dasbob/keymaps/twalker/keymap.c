2eda759b918944ef7158e76640ea26e9c45ddd04
tim walker <tim.twalker@gmail.com>
2024-03-16 08:06:06 -0700

    Add macro tri layer on outer thumbs

--- keyboards/dasbob/keymaps/twalker/keymap.c
+++ keyboards/dasbob/keymaps/twalker/keymap.c
@@ -316,6 +316,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   return true;
 }
+
+// macro layer on simultaneous outer thumbs
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, NUM, WIN, MAC);
+}
+
 #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
