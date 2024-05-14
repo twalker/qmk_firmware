caec484152f9262737eb96408e007fe1d1830152
tim walker <tim.twalker@gmail.com>
2024-05-13 21:46:25 -0700

    add elite-c config

---
+++ keyboards/handwired/dactyl_minidox/keymaps/twalker/config.h
@@ -0,0 +1,70 @@
+/* Select hand configuration */
+/* serial.c configuration for split keyboard */
+#define USE_SERIAL
+
+/* Select hand configuration */
+/* serial.c configuration for split keyboard */
+// Activate double tap run/reset on elite_pi
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+// #define EE_HANDS
+#define MASTER_LEFT
+
+/*
+  elite_c == elite_pi pins:
+MATRIX_ROW_PINS { F0 == D16, F1 == D15, C7 == D14, D5 == D13 }
+MATRIX_COL_PINS { D4 == D4, C6 == D5, D7 == D6, E6 == D7, B4 == D8, B5 == D9 }
+*/
+// same elite_c for elite pi as my tractyl
+// row-driven
+#undef MATRIX_ROW_PINS
+#define MATRIX_ROW_PINS { F0, F1, C7, D5 }
+#undef MATRIX_COL_PINS
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4 }
+
+/* COL2ROW or ROW2COL */
+#undef DIODE_DIRECTION
+#define DIODE_DIRECTION ROW2COL
+
+/* PMW3360 settings. */
+
+// Enable use of pointing device on slave split.
+/* #define SPLIT_POINTING_ENABLE */
+// Pointing device is on the right split.
+/* #define POINTING_DEVICE_RIGHT */
+
+// Limits the frequency that the sensor is polled for motion.
+// #define POINTING_DEVICE_TASK_THROTTLE_MS 1
+
+#define POINTING_DEVICE_INVERT_Y
+#define ROTATIONAL_TRANSFORM_ANGLE  -15
+// #define PMW33XX_CPI 650 // sent in keymap based on OS.
+
+// | `PMW33XX_CS_PIN`             | (Required) Sets the Cable Select pin connected to the sensor.                               | `POINTING_DEVICE_CS_PIN` |
+// | `PMW33XX_CS_PINS`            | (Alternative) Sets the Cable Select pins connected to multiple sensors.                     | _not defined_            |
+// | `PMW33XX_CPI`                | (Optional) Sets counts per inch sensitivity of the sensor.                                  | _varies_                 |
+// | `PMW33XX_CLOCK_SPEED`        | (Optional) Sets the clock speed that the sensor runs at.                                    | `2000000`                |
+// | `PMW33XX_SPI_DIVISOR`        | (Optional) Sets the SPI Divisor used for SPI communication.                                 | _varies_                 |
+// | `PMW33XX_LIFTOFF_DISTANCE`   | (Optional) Sets the lift off distance at run time                                           | `0x02`                   |
+// | `ROTATIONAL_TRANSFORM_ANGLE` | (Optional) Allows for the sensor data to be rotated +/- 127 degrees directly in the sensor. | `0`                      |
+
+
+#define PMW33XX_CS_PIN F7
+#undef SPI_SCK_PIN
+#define SPI_SCK_PIN B1
+#undef SPI_MOSI_PIN
+#define SPI_MOSI_PIN B2
+#undef SPI_MISO_PIN
+#define SPI_MISO_PIN B3
+#define SPI_DRIVER SPID0
+// Sensor pins: SS to F7, SC to B1, MO to B2, MI to B3
+#undef SPI_SCK_PIN
+#define SPI_SCK_PIN B1
+#undef SPI_MOSI_PIN
+#define SPI_MOSI_PIN B2
+#undef SPI_MISO_PIN
+#define SPI_MISO_PIN B3
+
+
+// The CPI range is 50-16000, in increments of 50. Defaults to 2000 CPI.
+#define PMW33XX_CPI 900 // also sent in keymap based on OS.
+
---
+++ keyboards/handwired/dactyl_minidox/keymaps/twalker/rules.mk
@@ -0,0 +1,6 @@
+MCU = atmega32u4
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+# CONVERT_TO = elite_c
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3389
