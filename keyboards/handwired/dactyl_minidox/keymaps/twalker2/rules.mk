6c2803085d685d7e73a9990ee0f071668a750a89
tim walker <tim.twalker@gmail.com>
2024-11-18 20:46:26 -0800

    Initial twalker2 keymap

---
+++ keyboards/handwired/dactyl_minidox/keymaps/twalker2/rules.mk
@@ -0,0 +1,33 @@
+# MCU = atmega32u4
+# Bootloader selection
+# BOOTLOADER = atmel-dfu
+
+CONVERT_TO = helios # elite_pi
+PIN_COMPATIBLE = elite_c
+
+# CONVERT_TO = elite_c
+# POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3360
+# DIODE_DIRECTION = ROW2COL
+# DIODE_DIRECTION = COL2ROW
+
+IGNORE_MOD_TAP_INTERRUPT = yes
+TAPPING_FORCE_HOLD = no # Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+PERMISSIVE_HOLD = no
+
+# Caps word
+CAPS_WORD_ENABLE = yes
+BOTH_SHIFTS_TURNS_ON_CAPS_WORD = yes
+
+# Macros
+DYNAMIC_MACRO_ENABLE = yes
+
+EXTRAKEY_ENABLE = yes # enable/disable media key and volume control
+
+# space savings
+LTO_ENABLE = yes
+ENABLE_CTL_GUI_SWAP = no
+MOUSEKEY_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
