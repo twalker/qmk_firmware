1e9df29e527313ca398ca0a09d02ee479734bac9
Tim Walker <tim.twalker@gmail.com>
2025-11-21 20:45:26 -0800

    compiles, but mouse not working

--- keyboards/hillside/52/keymaps/twalker/config.h
+++ keyboards/hillside/52/keymaps/twalker/config.h
@@ -45,3 +45,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //     #define RGBLIGHT_SAT_STEP 17
 //     #define RGBLIGHT_VAL_STEP 17
 // #endif
+#ifdef PS2_DRIVER_BUSYWAIT
+# define PS2_CLOCK_PIN   D1
+# define PS2_DATA_PIN    D2
+#endif
+
+
+// #define PS2_CLOCK_PIN A8
+// #define PS2_DATA_PIN  A9
+#define PS2_PIO_USE_PIO1
+// #define PS2_CLOCK_PIN A8
+// #define PS2_DATA_PIN  A9
+
+// #define PS2_DATA_PIN    GP2
+// #define PS2_CLOCK_PIN   GP3
+
+#define PS2_CLOCK_PIN D1
+#define PS2_DATA_PIN  D2
